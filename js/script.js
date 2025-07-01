f3cc1cf4ba8904f93e2994c53588aca47dd071fc
Mohamed <mohamedghazy632@gmail.com>
2025-07-02 01:55:50 +0300

    Add project files

---
+++ js/script.js
@@ -0,0 +1,420 @@
+  const products = [
+    {
+      name: "Trevor Gta Tshirt",
+      price: 73.9,
+      currency: "EG",
+          gender: "Men", //
+      image: "img/t shirts/Admis blue tee.png",
+      type: "best",
+      category: "tshirts",
+      availability: "outOfStock",
+      size: ["M", "L"],
+    },
+    {
+      name: "Tshirt Silent Pain",
+      price: 73.9,
+      currency: "EG",
+          gender: "Men", //
+      image: "img/t shirts/Tshirt Silent Pain.png",
+      type: "new",
+      category: "tshirts",
+      availability: "inStock",
+      size: ["S", "M", "XL"],
+    },
+    {
+      name: "Tshirt D",
+      price: 78.9,
+      currency: "EG",
+          gender: "Men", //
+      image: "img/t shirts/Tshirt D.png",
+      type: "sale",
+      category: "tshirts",
+      availability: "inStock",
+      size: ["XS", "S"],
+    },
+    {
+      name: "Tshirt Suicide Squad",
+      price: 78.9,
+      currency: "EG",
+          gender: "Men", //
+      image: "img/t shirts/Tshirt Suicide Squad.png",
+      type: "sale",
+      category: "tshirts",
+      availability: "outOfStock",
+      size: ["M", "L"],
+    },
+    {
+      name: "Tshirt Itachi Uchiha",
+      price: 92.9,
+      currency: "EG",
+          gender: "Men", //
+      image: "img/t shirts/Tshirt Itachi Uchiha.png",
+      type: "new",
+      category: "tshirts",
+      availability: "inStock",
+      size: ["XS", "S", "M"],
+    },
+    {
+      name: "Tshirt Rise Again",
+      price: 92.9,
+      currency: "EG",
+          gender: "Men", //
+      image: "img/t shirts/Tshirt Rise Again.png",
+      type: "best",
+      category: "tshirts",
+      availability: "outOfStock",
+      size: ["L", "XL"],
+    },
+    {
+      name: "Arabian Touch",
+      price: 1200,
+      currency: "EG",
+          gender: "Men", //
+      image: "img/t shirts/Arabian Touch.png",
+      type: "new",
+      category: "tshirts",
+      availability: "inStock",
+      size: ["M"],
+    },
+    {
+      name: "أحلامك أكبر من خوفك",
+      price: 78.9,
+      currency: "EG",
+          gender: "Men", //
+      image: "img/t shirts/asrar.png",
+      type: "sale",
+      category: "tshirts",
+      availability: "outOfStock",
+      size: ["S", "L"],
+    },
+    {
+      name: "Mens Polo Shirt",
+      price: 78.9,
+      currency: "EG",
+          gender: "Men", //
+      image: "img/t shirts/Mens Polo Shirt.png",
+      type: "best",
+      category: "tshirts",
+      availability: "inStock",
+      size: ["XS", "M"],
+    },
+    {
+      name: "Jogger Shorts",
+      price: 78.9,
+      currency: "EG",
+          gender: "Men", //
+      image: "img/t shirts/Jogger Shorts.png",
+      type: "new",
+      category: "shorts",
+      availability: "inStock",
+      size: ["M", "L"],
+    },
+    {
+      name: "AC Hoodie BLACK",
+      price: 133,
+      currency: "EG",
+          gender: "Men", //
+      image: "img/t shirts/hoodie-white.png",
+      type: "sale",
+      category: "hoodies",
+      availability: "outOfStock",
+      size: ["S", "M", "L"],
+    },
+    {
+      name: "New Hoodie White",
+      price: 133,
+      currency: "EG",
+          gender: "Men", //
+      image: "img/t shirts/hoodie-gray.png",
+      type: "new",
+      category: "hoodies",
+      availability: "inStock",
+      size: ["L", "XL"],
+    },
+    {
+      name: "New Hoodie RED",
+      price: 133,
+      currency: "EG",
+          gender: "Men", //
+      image: "img/t shirts/hoodie-red.png",
+      type: "new",
+      category: "hoodies",
+      availability: "inStock",
+      size: ["M", "L"],
+    },
+    {
+      name: "New Hoodie Purple",
+      price: 133,
+      currency: "EG",
+          gender: "Men", //
+      image: "img/t shirts/hoodie-purple.png",
+      type: "sale",
+      category: "hoodies",
+      availability: "inStock",
+      size: ["S", "M"],
+    },
+    {
+      name: "New Hoodie Turquoise",
+      price: 133,
+      currency: "EG",
+          gender: "Men", //
+      image: "img/t shirts/hoodie-Turquoise.png",
+      type: "new",
+      category: "hoodies",
+      availability: "outOfStock",
+      size: ["M", "L", "XL"],
+    },
+    {
+      name: "Adidas Originals Pants White",
+      price: 100,
+      currency: "EG",
+          gender: "Men", //
+      image: "img/t shirts/pants-White.png",
+      type: "best",
+      category: "pants",
+      availability: "inStock",
+      size: ["S", "M"],
+    },
+    {
+      name: "Adidas Originals Pants Blue",
+      price: 100,
+      currency: "EG",
+          gender: "Men", //
+      image: "img/t shirts/pants-Blue.png",
+      type: "sale",
+      category: "pants",
+      availability: "outOfStock",
+      size: ["M"],
+    },
+    {
+      name: "Adidas Originals Pants Red",
+      gender: "Men", //
+      price: 100,
+      currency: "EG",
+          gender: "Men", //
+      image: "img/t shirts/pants-Red.png",
+      type: "new",
+      category: "pants",
+      availability: "inStock",
+      size: ["M", "L", "XL"],
+    },
+    {
+      name: "Adidas Originals Pants Purple",
+      price: 100,
+      currency: "EG",
+          gender: "Men", //
+      image: "img/t shirts/pants-purple.png",
+      type: "sale",
+      category: "pants",
+      availability: "outOfStock",
+      size: ["S", "L"],
+    },
+    {
+      name: "Adidas Originals Pants Turquoise",
+      price: 100,
+      currency: "EG",
+          gender: "Men", //
+      image: "img/t shirts/pants-Turquoise.png",
+      type: "best",
+      category: "pants",
+      availability: "inStock",
+      size: ["XS", "M"],
+    },
+    {
+      name: "Jean Jackets For Men |Denim Jackets withTshirt",
+      price: 450,
+      currency: "EG",
+          gender: "Men", //
+      image: "img/t shirts/Jackets-Jean-Denim.png",
+      type: "new",
+      category: "hoodies",
+      availability: "inStock",
+      size: ["M", "L", "XL"],
+    },
+    {
+      name: "Jackets For Men |Black Jackets withTshirt",
+      price: 400,
+      currency: "EG",
+          gender: "Men", //
+      image: "img/t shirts/Black-Jackets-withTshirt.png",
+      type: "sale",
+  categories: ["hoodies", "men"], 
+      availability: "inStock",
+      size: ["XS", "S", "M"],
+    },
+    {
+      name: "Yeezy Shoes | Adidas Yeezy Boost Neon",
+      price: 1200,
+      currency: "EG",
+          gender: "men", //
+      image: "img/t shirts/Yeezy Boost.png",
+      type: "sale",
+  categories: ["hoodies", "men"], 
+      availability: "inStock",
+      size: ["XS", "S", "M"],
+    },
+  ];
+  if (document.body.id === "index-page") {
+    // Function to render products
+    function renderProducts(products) {
+      const productContainer = document.querySelector(".row");
+      productContainer.innerHTML = "";
+
+      // Limit the number of products to 4
+      const limitedProducts = products.slice(0, 4);
+
+      limitedProducts.forEach((product) => {
+        const productHTML = `
+              <a style="text-decoration: none; color: inherit" href="./Shop.html">
+                  <div class="col card-home" data-type="${product.type}">
+          <div class="card no-border">
+            <span class="badge bg-secondary position-absolute top-0 start-0 m-2">${
+              product.type === "new"
+                ? "New"
+                : product.type === "sale"
+                ? "Sale"
+                : "Best"
+            }</span>
+            <img src="${product.image}" class="card-img-top img-bg" alt="${
+          product.name
+        }" />
+            <div class="card-body text-center">
+              <h5 class="card-title">${product.name}</h5>
+              <p class="card-text">${product.price} EG</p>
+            </div>
+          </div>
+        </div>
+              </a>
+
+    
+      `;
+        productContainer.innerHTML += productHTML;
+      });
+    }
+
+    // Filter products by type
+    function filterProducts(type) {
+      const filteredProducts = type
+        ? products.filter((product) => product.type === type)
+        : products;
+      renderProducts(filteredProducts);
+    }
+
+    // Event listeners for filter buttons
+    const filterButtons = document.querySelectorAll(".filter-btn");
+    filterButtons.forEach((button) => {
+      button.addEventListener("click", (e) => {
+        const type = e.target.dataset.type;
+        filterProducts(type);
+      });
+    });
+
+    // Render all products initially
+    renderProducts(products);
+
+    document.addEventListener("DOMContentLoaded", function () {
+      const newsletterForm = document.querySelector(".newsletter form");
+
+      newsletterForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const emailInput = newsletterForm.querySelector('input[type="email"]');
+        const email = emailInput.value;
+
+        if (email) {
+          alert(`Thank you for subscribing with ${email}!`);
+          emailInput.value = "";
+        } else {
+          alert("Please enter a valid email address.");
+        }
+      });
+    });
+  }
+  function filterProducts() {
+    const categoryCheckboxes = document.querySelectorAll(
+      '.form-check-input[type="checkbox"]:checked'
+    );
+    const selectedCategories = Array.from(categoryCheckboxes).map(cb => cb.value);
+
+    const genderCheckboxes = document.querySelectorAll(
+      '.gender-checkbox[type="checkbox"]:checked'
+    );
+    const selectedGenders = Array.from(genderCheckboxes).map(cb => cb.value);
+
+    const sizeCheckboxes = document.querySelectorAll(".custom-size-btn");
+    const selectedSizes = Array.from(sizeCheckboxes)
+      .filter(cb => cb.checked)
+      .map(cb => cb.value);
+
+    filteredProducts = products
+      .filter(
+        product =>
+          selectedCategories.length === 0 ||
+          selectedCategories.includes(product.category)
+      )
+      .filter(
+        product =>
+          selectedGenders.length === 0 ||
+          selectedGenders.includes(product.gender)
+      )
+      .filter(
+        product =>
+          selectedAvailability === "all" ||
+          product.availability === selectedAvailability
+      )
+      .filter(
+        product =>
+          selectedSizes.length === 0 ||
+          product.size.some(size => selectedSizes.includes(size))
+      )
+      .filter(product => product.price <= maxPrice);
+
+    sortProducts();
+    currentPage = 1;
+    renderProducts(currentPage);
+  }
+
+
+  if (document.body.id === "about-page") {
+    //ABOUT US INSTA PICS SELECTION //
+    document.querySelectorAll(".overlay").forEach((overlay, index) => {
+      overlay.addEventListener("click", () => {
+        const urls = [
+
+        ];
+        window.open(urls[index], "_blank");
+      });
+    });
+  }
+  if (document.body.id === "contact-page") {
+    document.addEventListener("DOMContentLoaded", function () {
+      const contactForm = document.querySelector("form");
+
+      contactForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const nameInput = contactForm.querySelector("#name");
+        const emailInput = contactForm.querySelector("#email");
+        const phoneInput = contactForm.querySelector("#phone");
+        const messageInput = contactForm.querySelector("#message");
+
+        const name = nameInput.value;
+        const email = emailInput.value;
+        const phone = phoneInput.value;
+        const message = messageInput.value;
+
+        if (name && email && phone && message) {
+          alert(
+            `Thank you for contacting us, ${name}! We will respond to your message soon.`
+          );
+          nameInput.value = "";
+          emailInput.value = "";
+          phoneInput.value = "";
+          messageInput.value = "";
+        } else {
+          alert("Please fill out all fields.");
+        }
+        window.location.href = "./index.html";
+      });
+    });
+  }
